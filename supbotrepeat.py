a0562174e36b68483306d6632d9dce5a4b02eb9f
SuperiorRup <46254794+SuperiorRup@users.noreply.github.com>
2019-01-27 09:32:16 +0530

    Update supbotrepeat.py

--- supbotrepeat.py
+++ supbotrepeat.py
@@ -11,7 +11,7 @@ async def say(ctx, *args):
     mesg = ' '.join(args)
     return await Mike.say(mesg)
 
-Mike.run('NTI4NTA0MzMyMjYyMzA5ODg4.DwoIKQ.P9HY47BQ3wD5BtZz3JH6kUzOtwA')
+Mike.run('NTM3NTgyMzAxMzEyMzE5NDkw.Dy68_A.ZbHplVBXgdVKXa2Kgfo_h4Y30tI')
 
 
 
